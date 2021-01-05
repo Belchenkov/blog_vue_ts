7740b3084ee0ac52b2ed41461d9ced72c2d13ada
Aleksey Belchenkov <u608110@gmail.com>
2021-01-05 15:27:31 +0300

    Add Footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
     <HeaderComponent v-if="$route.name !== 'Login' && $route.name !== '404'" />
-    <router-view></router-view>
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+    <FooterComponent v-if="$route.name !== 'Login' && $route.name !== '404'" />
   </div>
 </template>
 
 <script>
 import HeaderComponent from "./components/layout/HeaderComponent";
+import FooterComponent from "./components/layout/FooterComponent";
+
 import "@/assets/style/global.scss";
 
 export default {
   name: "app",
   components: {
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   }
 };
 </script>
