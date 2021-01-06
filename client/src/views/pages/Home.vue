5b09a25a9acafd40c7564138ff41547f468c725b
Aleksey Belchenkov <u608110@gmail.com>
2021-01-06 12:45:02 +0300

    Search & Change View

--- client/src/views/pages/Home.vue
+++ client/src/views/pages/Home.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="home">
-    <div v-if="articles">
+    <el-row type="flex" justify="center">
+      <el-col :span="12" class="articles-control">
+        <el-input :placeholder="$t('search')" v-model="searchValue"></el-input>
+        <el-tooltip
+          effect="dark"
+          :content="$t('tooltip')"
+          placement="top-start"
+        >
+          <el-button
+            type="primary"
+            :icon="viewMode ? 'el-icon-s-grid' : 'el-icon-menu'"
+            circle
+            class="viewModeBtn"
+            @click="changeView"
+          ></el-button>
+        </el-tooltip>
+      </el-col>
+    </el-row>
+    <div v-if="articles" :class="viewMode ? '' : 'block-view'">
       <ArticleView
         v-for="item in articles"
         :key="item.id"
         :item="item"
+        :search="searchValue"
+        :viewMode="viewMode"
       ></ArticleView>
     </div>
     <div v-else>
@@ -23,6 +43,8 @@ import ArticleView from "../../components/ArticleView.vue";
   }
 })
 export default class Home extends Vue {
+  searchValue: string = "";
+  viewMode: boolean = true;
   articles = [
     {
       id: 1,
@@ -58,5 +80,31 @@ export default class Home extends Vue {
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis viverra sapien. Mauris vehicula est vitae sodales aliquam. Aliquam sem nisl, pretium et justo vitae, sagittis dictum nunc. Nulla facilisi. Sed ultrices nisl lorem, in consequat urna facilisis a. Sed eu hendrerit risus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis viverra sapien. Mauris vehicula est vitae sodales aliquam. Aliquam sem nisl, pretium et justo vitae, sagittis dictum nunc. Nulla facilisi. Sed ultrices nisl lorem, in consequat urna facilisis a. Sed eu hendrerit risus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis viverra sapien. Mauris vehicula est vitae sodales aliquam. Aliquam sem nisl, pretium et justo vitae, sagittis dictum nunc. Nulla facilisi. Sed ultrices nisl lorem, in consequat urna facilisis a. Sed eu hendrerit risus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis viverra sapien. Mauris vehicula est vitae sodales aliquam. Aliquam sem nisl, pretium et justo vitae, sagittis dictum nunc. Nulla facilisi. Sed ultrices nisl lorem, in consequat urna facilisis a. Sed eu hendrerit risus."
     }
   ];
+  changeView() {
+    this.viewMode = !this.viewMode;
+  }
 }
 </script>
+
+<style lang="scss">
+.viewModeBtn {
+  margin-top: 10px;
+}
+.articles-control {
+  .el-input {
+    display: inline-block;
+    width: 250px;
+    margin: 0 5px 0 0;
+    @media (max-width: 768px) {
+      display: block;
+      width: 100%;
+      margin: 0 0 10px 0;
+    }
+  }
+}
+.block-view {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+}
+</style>
